a658382832575a19fcce7f349eaba74f0df4f178
sarathsidharth <muscbtz@gmail.com>
2021-09-18 16:33:31 +0530

    cart api service added

--- src/app/component/products/products.component.ts
+++ src/app/component/products/products.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/service/api.service';
+import { CartService } from 'src/app/service/cart.service';
 
 @Component({
   selector: 'app-products',
@@ -9,10 +10,19 @@ import { ApiService } from 'src/app/service/api.service';
 export class ProductsComponent implements OnInit {
   
   public productList:any;
-  constructor(private api:ApiService) { }
+  constructor(private api:ApiService,private cartservice:CartService) { }
 
   ngOnInit(): void {
-    this.api.getProduct().subscribe((res)=>{this.productList=res})
+    this.api.getProduct().subscribe((res)=>{this.productList=res;
+    this.productList.forEach((a:any)=>{
+      Object.assign(a,{quantity:1,total:a.price})
+    });
+    })
   }
+
+  addtocart(item:any){
+    this.cartservice.addToCart(item);
+  }
+
 
 }
