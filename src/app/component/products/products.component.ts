41f9cda37bc5b77464edadcd498cfc0cc9df3995
sarathsidharth <muscbtz@gmail.com>
2021-09-20 13:18:55 +0530

    pipe added seraching products filtering products are implemented

--- src/app/component/products/products.component.ts
+++ src/app/component/products/products.component.ts
@@ -10,13 +10,25 @@ import { CartService } from 'src/app/service/cart.service';
 export class ProductsComponent implements OnInit {
   
   public productList:any;
+  searchKey:string="";
+  public filterCategory:any;
+
   constructor(private api:ApiService,private cartservice:CartService) { }
 
   ngOnInit(): void {
-    this.api.getProduct().subscribe((res)=>{this.productList=res;
-    this.productList.forEach((a:any)=>{
+    this.api.getProduct().subscribe((res)=>{
+      this.productList=res;
+      this.filterCategory=res;
+      this.productList.forEach((a:any)=>{
+      if(a.category ==="women's clothing"||a.category ==="men's clothing"){
+        a.category="fashion"
+      }
       Object.assign(a,{quantity:1,total:a.price})
+      console.log(this.productList)
     });
+    this.cartservice.search.subscribe((val:any)=>{
+      this.searchKey=val;
+    })
     })
   }
 
@@ -24,5 +36,11 @@ export class ProductsComponent implements OnInit {
     this.cartservice.addToCart(item);
   }
 
+  filter(category:string){
+    this.filterCategory=this.productList.filter((a:any)=>{
+      if(a.category==category||category=='')
+      return a;
+    })
+  }
 
 }
